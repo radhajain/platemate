e562f36baea0808a8ec4709134dcdacb7a93aff1
Radha Jain <radha.m.jain@gmail.com>
2026-01-20 11:48:35 -0500

    update supabase

--- src/services/supabase/middleware.ts
+++ src/services/supabase/middleware.ts
@@ -1,4 +1,4 @@
-import { createServerClient } from '@supabase/ssr';
+import { createServerClient, type CookieOptions } from '@supabase/ssr';
 import { NextResponse, type NextRequest } from 'next/server';
 
 export async function updateSession(request: NextRequest) {
@@ -14,19 +14,25 @@ export async function updateSession(request: NextRequest) {
 				getAll() {
 					return request.cookies.getAll();
 				},
-				setAll(cookiesToSet) {
+				setAll(
+					cookiesToSet: {
+						name: string;
+						value: string;
+						options: CookieOptions;
+					}[],
+				) {
 					cookiesToSet.forEach(({ name, value }) =>
-						request.cookies.set(name, value)
+						request.cookies.set(name, value),
 					);
 					supabaseResponse = NextResponse.next({
 						request,
 					});
 					cookiesToSet.forEach(({ name, value, options }) =>
-						supabaseResponse.cookies.set(name, value, options)
+						supabaseResponse.cookies.set(name, value, options),
 					);
 				},
 			},
-		}
+		},
 	);
 
 	// IMPORTANT: Avoid writing any logic between createServerClient and
